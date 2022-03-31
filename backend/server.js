c325595a7124e733438e57604520647cf3f9d3d2
davincecode <hyperscript.ca@gmail.com>
2022-03-31 17:45:43 -0600

    Added dependencies

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,17 @@ const PORT = process.env.PORT || 5012
 const cors = require("cors")
 const mongoose = require("mongoose")
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB... 📀")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
 app.use(cors())
 app.use(morgan("dev"))
