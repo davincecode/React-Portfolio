1e1010ea28c26c14451aa1ea288877e3ec9a72df
davincecode <hyperscript.ca@gmail.com>
2022-03-31 17:29:34 -0600

    Initial setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,26 @@
 require("dotenv").config()
 
 const express = require("express")
+const morgan = require("morgan")
 const app = express()
+const PORT = process.env.PORT || 5012
+const cors = require("cors")
+const mongoose = require("mongoose")
 
-const PORT = process.env.PORT || 5000
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+app.use(cors())
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
+
+app.get("/", function (req, res) {
+  res.send("hello, world!")
+})
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT} 🍺`)
