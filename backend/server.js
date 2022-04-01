1c35a0b9c392b9c85fd04fb199fc44d45687567b
davincecode <hyperscript.ca@gmail.com>
2022-04-01 15:59:55 -0600

    Added routes

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ const mongoose = require("mongoose")
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
 const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
 
 mongoose
   .connect(process.env.MONGODB_URI, {
@@ -34,7 +36,9 @@ app.get("/api/test", function (req, res) {
 
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
-app.use("/api/product", productRoute)
+app.use("/api/products", productRoute)
+app.use("/api/carts", cartRoute)
+app.use("/api/orders", orderRoute)
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT} 🍺`)
