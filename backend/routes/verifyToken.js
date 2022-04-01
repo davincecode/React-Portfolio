ef2bb01507446009964d0c8455105ff8154edd99
davincecode <hyperscript.ca@gmail.com>
2022-04-01 15:31:20 -0600

    Added User auth, tokens, routes

--- backend/routes/verifyToken.js
+++ backend/routes/verifyToken.js
@@ -5,14 +5,12 @@ const verifyToken = (req, res, next) => {
   if (authHeader) {
     const token = authHeader.split(" ")[1]
     jwt.verify(token, process.env.JWT_SEC, (err, user) => {
-      if (err) {
-        return res.status(401).json({ message: "Invalid Token" })
-      }
+      if (err) res.status(403).json("Token is not valid!")
       req.user = user
       next()
     })
   } else {
-    return res.status(401).json({ Authorization: "Token is missing" })
+    return res.status(401).json("You are not authenticated!")
   }
 }
 
@@ -21,9 +19,23 @@ const verifyTokenAndAuthorization = (req, res, next) => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next()
     } else {
-      res.status(403).json("You are not authorized to do this")
+      res.status(403).json("You are not alowed to do that!")
     }
   })
 }
 
-module.exports = { verifyToken, verifyTokenAndAuthorization }
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next()
+    } else {
+      res.status(403).json("You are not alowed to do that!")
+    }
+  })
+}
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+}
