b76847bf9c365318e7e9334978700b9afaed1577
davincecode <hyperscript.ca@gmail.com>
2022-04-10 01:47:26 -0600

    Update recent work mobile responsive

--- frontend/src/components/Projects/index.jsx
+++ frontend/src/components/Projects/index.jsx
@@ -8,8 +8,8 @@ export default function index() {
         <SectionTitle>RECENT WORK</SectionTitle>
         <section className="overflow-hidden text-gray-700 ">
           <div className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
-            <div className="flex flex-wrap -m-1 md:-m-2">
-              <div className="flex flex-wrap w-1/3">
+            <div className="flex flex-col flex-wrap md:flex-row md:-m-2">
+              <div className="flex flex-wrap w-full md:w-1/3">
                 <div className="w-full p-1 md:p-2" data-aos="fade-up">
                   <img
                     alt="gallery"
@@ -18,7 +18,7 @@ export default function index() {
                   />
                 </div>
               </div>
-              <div className="flex flex-wrap w-1/3">
+              <div className="flex flex-wrap w-full md:w-1/3">
                 <div className="w-full p-1 md:p-2" data-aos="fade-up">
                   <img
                     alt="gallery"
@@ -27,7 +27,7 @@ export default function index() {
                   />
                 </div>
               </div>
-              <div className="flex flex-wrap w-1/3">
+              <div className="flex flex-wrap w-full md:w-1/3">
                 <div className="w-full p-1 md:p-2" data-aos="fade-up" data-aos-delay="500">
                   <img
                     alt="gallery"
@@ -36,7 +36,7 @@ export default function index() {
                   />
                 </div>
               </div>
-              <div className="flex flex-wrap w-1/3">
+              <div className="flex flex-wrap w-full md:w-1/3">
                 <div className="w-full p-1 md:p-2" data-aos="fade-up" data-aos-delay="1000">
                   <img
                     alt="gallery"
@@ -45,7 +45,7 @@ export default function index() {
                   />
                 </div>
               </div>
-              <div className="flex flex-wrap w-1/3">
+              <div className="flex flex-wrap w-full md:w-1/3">
                 <div className="w-full p-1 md:p-2" data-aos="fade-up" data-aos-delay="1500">
                   <img
                     alt="gallery"
@@ -54,7 +54,7 @@ export default function index() {
                   />
                 </div>
               </div>
-              <div className="flex flex-wrap w-1/3">
+              <div className="flex flex-wrap w-full md:w-1/3">
                 <div className="w-full p-1 md:p-2" data-aos="fade-up" data-aos-delay="2000">
                   <img
                     alt="gallery"
