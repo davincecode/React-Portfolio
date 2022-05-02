57ed29f61c7bd458d79ef36cbbaa43a50aae3ed1
davincecode <hyperscript.ca@gmail.com>
2022-05-01 21:58:00 -0600

    Update server

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,9 @@ app.use("/api/checkout", stripeRoute)
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  })
 }
 
 app.listen(PORT, () => {
