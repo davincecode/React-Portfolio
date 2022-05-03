74cd2f95c7ffd760c1644584f211a4bee9672aa9
davincecode <hyperscript.ca@gmail.com>
2022-05-02 19:33:23 -0600

    Update projects list

--- frontend/src/Data/projectsList.jsx
+++ frontend/src/Data/projectsList.jsx
@@ -35,8 +35,8 @@ const projectsList = [
   {
     title: 'Tweeter Project',
     image: '/images/twitter.gif',
-    description: 'Tweeter is a simple, single-page Twitter clone. Tech Stack used to build this project are HTML, CSS, JS, jQuery and AJAX for the frontend, and Node, Express, body-parser for the backend.',
-    stack: 'Gatsby, React, GraphQL, Instagram API, Styled-Components.',
+    description: 'Tweeter is a simple, single-page Twitter clone. Made it to be responsive and mobile-friendly.',
+    stack: 'HTML, CSS, JS, jQuery, AJAX, Node, Express, body-parser',
     link1: "/",
     link2: "https://github.com/davincecode/tweeter"
   },
@@ -44,6 +44,7 @@ const projectsList = [
     title: 'Tiny App Project',
     image: '/images/tinyapp.png',
     description: 'TinyApp is a full stack web application that allows users to shorten long URLs (similar to bit.ly). TinyApp is built with Node and Express for the backend and HTML, EJS and Bootsrap for the frontend.',
+    stack: 'Node, Express, EJS, bycrypt, Bootstrap, Mocha and Chai.',
     link1: "/",
     link2: "https://github.com/davincecode/tinyapp"
   },
